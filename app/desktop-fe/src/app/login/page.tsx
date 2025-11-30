394628d3b850d5cef80168a873a439e89478e9f9
Paul Thanataweenont <paul@chronos.email>
2025-11-30 00:00:00 -0700

    feat: enhance login page with social login options and input icons, add new styles and icons

--- app/desktop-fe/src/app/login/page.tsx
+++ app/desktop-fe/src/app/login/page.tsx
@@ -5,6 +5,9 @@ import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import styles from "./page.module.scss";
 import Logo from "@/components/logo";
+import GoogleLogo from "@/components/google-logo";
+import FacebookLogo from "@/components/facebook-logo";
+import { HiOutlineMail, HiOutlineLockClosed  } from "react-icons/hi";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -39,48 +42,63 @@ export default function LoginPage() {
       <form className={styles.loginForm} onSubmit={onSubmit}>
         <div className={styles.loginData}>
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+          <div className={styles.inputWrapper}>
+            <HiOutlineMail className={styles.inputIcon} />
+            <input
+              id="email"
+              type="email"
+              value={email}
+              placeholder="you@example.com"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
         </div>
         <div className={styles.loginData}>
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <div className={styles.inputWrapper}>
+            <HiOutlineLockClosed className={styles.inputIcon} />
+            <input
+              id="password"
+              type="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
         </div>
-        <button className={styles.loginButton}
-          type="submit"
-        >
+        <button className={styles.loginButton} type="submit">
           Login
-        </button>            
-        <div className={styles.buttonWrapper}>      
+        </button>
+        <div className={styles.divider}>
+          <span className={styles.orText}>Or continue with</span>
+        </div>
+        <div className={styles.buttonWrapper}>
           <button
-            className={styles.googleButton}
+            className={styles.socialButton}
             type="button"
             onClick={() => signIn("google", { callbackUrl: "/home" })}
           >
+            <GoogleLogo width={24} height={24} />
             Continue with Google
           </button>
           <button
-            className={styles.signupButton}
+            className={styles.socialButton}
             type="button"
-            onClick={() => router.push("/signup")}
+            onClick={() => signIn("facebook", { callbackUrl: "/home" })}
           >
-            Create account
-          </button>
-          {/* <button type="button" onClick={() => signIn("facebook", { callbackUrl: "/home" })}>
+            <FacebookLogo width={24} height={24} />
             Continue with Facebook
-          </button>           */}
+          </button>
         </div>
+        <button
+          className={styles.signupButton}
+          type="button"
+          onClick={() => router.push("/signup")}
+        >
+          Don't have an account? Sign up
+        </button>
       </form>
     </div>
   );
